29db7965ad1ca792a6d84ab9dfe22aa068e9587c
Cody Tseng <64680921+CodyTseng@users.noreply.github.com>
2024-06-10 23:24:38 +0800

    feat: add tag condition for restriction (#15)

--- src/common/rule.ts
+++ src/common/rule.ts
@@ -1,68 +1,13 @@
-import { Filter as NostrFilter } from '@nostr-relay/common'
-
 export const RULE_ACTION = {
   BLOCK: 'block',
   ALLOW: 'allow'
 } as const
 export type TRuleAction = (typeof RULE_ACTION)[keyof typeof RULE_ACTION]
 
-export type TRuleFilter = Omit<NostrFilter, 'ids' | 'search' | 'limit' | 'since' | 'until'>
-
 export const RULE_CONDITION_FIELD_NAME = {
   AUTHOR: 'author',
   KIND: 'kind',
-  a: '#a',
-  b: '#b',
-  c: '#c',
-  d: '#d',
-  e: '#e',
-  f: '#f',
-  g: '#g',
-  h: '#h',
-  i: '#i',
-  j: '#j',
-  k: '#k',
-  l: '#l',
-  m: '#m',
-  n: '#n',
-  o: '#o',
-  p: '#p',
-  q: '#q',
-  r: '#r',
-  s: '#s',
-  t: '#t',
-  u: '#u',
-  v: '#v',
-  w: '#w',
-  x: '#x',
-  y: '#y',
-  z: '#z',
-  A: '#A',
-  B: '#B',
-  C: '#C',
-  D: '#D',
-  E: '#E',
-  F: '#F',
-  G: '#G',
-  H: '#H',
-  I: '#I',
-  J: '#J',
-  K: '#K',
-  L: '#L',
-  M: '#M',
-  N: '#N',
-  O: '#O',
-  P: '#P',
-  Q: '#Q',
-  R: '#R',
-  S: '#S',
-  T: '#T',
-  U: '#U',
-  V: '#V',
-  W: '#W',
-  X: '#X',
-  Y: '#Y',
-  Z: '#Z'
+  TAG: 'tag'
 } as const
 export const RULE_CONDITION_FIELD_NAMES = Object.values(RULE_CONDITION_FIELD_NAME)
 export const MAX_RULE_CONDITIONS = RULE_CONDITION_FIELD_NAMES.length
--- src/main/plugins/restriction.plugin.ts
+++ src/main/plugins/restriction.plugin.ts
@@ -4,17 +4,18 @@ import {
   HandleMessagePlugin,
   HandleMessageResult,
   IncomingMessage,
-  MessageType
+  MessageType,
+  Event as NostrEvent
 } from '@nostr-relay/common'
 import { createOutgoingOkMessage } from '@nostr-relay/core'
 import { nip19 } from 'nostr-tools'
-import {
-  RULE_ACTION,
-  RULE_CONDITION_FIELD_NAME,
-  TRule,
-  TRuleAction,
-  TRuleFilter
-} from '../../common/rule'
+import { RULE_ACTION, RULE_CONDITION_FIELD_NAME, TRule, TRuleAction } from '../../common/rule'
+
+export type TRuleFilter = {
+  authors: string[]
+  kinds: number[]
+  tags: string[][]
+}
 
 const BLOCKED_MESSAGE = 'blocked:'
 
@@ -30,12 +31,10 @@ export class RestrictionPlugin implements HandleMessagePlugin {
     if (message[0] !== MessageType.EVENT) return next()
 
     const event = message[1]
-    const isMatchingFilter = this.filters.some((filter) =>
-      EventUtils.isMatchingFilter(event, filter)
-    )
+    const isMatchingFilters = this.isMatchingFilters(event)
     const needBlock =
-      (this.defaultAction === RULE_ACTION.BLOCK && !isMatchingFilter) ||
-      (this.defaultAction === RULE_ACTION.ALLOW && isMatchingFilter)
+      (this.defaultAction === RULE_ACTION.BLOCK && !isMatchingFilters) ||
+      (this.defaultAction === RULE_ACTION.ALLOW && isMatchingFilters)
 
     if (needBlock) {
       ctx.sendMessage(createOutgoingOkMessage(event.id, false, BLOCKED_MESSAGE))
@@ -50,15 +49,43 @@ export class RestrictionPlugin implements HandleMessagePlugin {
 
   updateFiltersByRules(rules: TRule[]) {
     this.filters = rules.map((rule) => {
-      const filter: TRuleFilter = {}
+      const filter: TRuleFilter = {
+        authors: [],
+        kinds: [],
+        tags: []
+      }
       rule.conditions.forEach((condition) => {
         if (!condition.fieldName || condition.values.length <= 0) return
-        if (condition.fieldName !== RULE_CONDITION_FIELD_NAME.AUTHOR) {
-          filter[condition.fieldName] = condition.values
+
+        if (condition.fieldName === RULE_CONDITION_FIELD_NAME.AUTHOR) {
+          filter.authors = condition.values.map((v) => nip19.decode(v as string).data as string)
+        } else if (condition.fieldName === RULE_CONDITION_FIELD_NAME.KIND) {
+          filter.kinds = condition.values as number[]
+        } else if (condition.fieldName === RULE_CONDITION_FIELD_NAME.TAG) {
+          filter.tags.push(condition.values as string[])
         }
-        filter.authors = condition.values.map((v) => nip19.decode(v as string).data as string)
       })
       return filter
+    })
+  }
+
+  private isMatchingFilters(event: NostrEvent) {
+    return this.filters.some((filter) => {
+      if (filter.kinds.length && !filter.kinds.includes(event.kind)) {
+        return false
+      }
+
+      const author = EventUtils.getAuthor(event, false)
+      if (filter.authors.length && !filter.authors.includes(author)) {
+        return false
+      }
+
+      if (filter.tags.length) {
+        const eventTags = event.tags.map(([tagName, tagValue]) => `${tagName}:${tagValue}`)
+        return filter.tags.every((arr) => arr.some((item) => eventTags.includes(item)))
+      }
+
+      return true
     })
   }
 }
