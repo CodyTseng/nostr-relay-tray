07068173dba7e0e8f952882fcf878c43b650dfe7
Cody Tseng <64680921+CodyTseng@users.noreply.github.com>
2024-06-23 14:20:34 +0800

    feat: use framless windows on macOS (#18)

--- src/main/index.ts
+++ src/main/index.ts
@@ -43,7 +43,8 @@ function createWindow(): void {
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
-    }
+    },
+    titleBarStyle: process.platform === 'darwin' ? 'hiddenInset' : undefined
   })
 
   // Open the DevTools.
