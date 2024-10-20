469eb510ff0d50010b573b1f36f5a63939f48765
codytseng <codytseng98@gmail.com>
2024-10-20 20:10:15 +0800

    image zoom

--- src/renderer/src/viewer/components/ImageGallery/index.tsx
+++ src/renderer/src/viewer/components/ImageGallery/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Photo, RowsPhotoAlbum } from 'react-photo-album'
 import Lightbox from 'yet-another-react-lightbox'
+import Zoom from 'yet-another-react-lightbox/plugins/zoom'
 
 export default function ImageGallery({
   className,
@@ -51,9 +52,11 @@ export default function ImageGallery({
       <Lightbox
         index={index}
         slides={slides}
+        plugins={[Zoom]}
         open={index >= 0}
         close={() => setIndex(-1)}
         controller={{ closeOnBackdropClick: true, closeOnPullUp: true, closeOnPullDown: true }}
+        styles={{ toolbar: { paddingTop: '2.25rem' } }}
       />
     </div>
   )
