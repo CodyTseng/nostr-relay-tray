3267654b495451260a91dc9eabbd86b902203374
codytseng <codytseng@outlook.com>
2024-10-22 14:16:05 +0800

    fix timestamp display

--- src/renderer/src/lib/timestamp.ts
+++ src/renderer/src/lib/timestamp.ts
@@ -5,22 +5,22 @@ export function formatTimestamp(timestamp: number) {
   const now = dayjs()
 
   const diffMonth = now.diff(time, 'month')
-  if (diffMonth > 1) {
+  if (diffMonth >= 1) {
     return time.format('MMM D, YYYY')
   }
 
   const diffDay = now.diff(time, 'day')
-  if (diffDay > 1) {
+  if (diffDay >= 1) {
     return `${diffDay} days ago`
   }
 
   const diffHour = now.diff(time, 'hour')
-  if (diffHour > 1) {
+  if (diffHour >= 1) {
     return `${diffHour} hours ago`
   }
 
   const diffMinute = now.diff(time, 'minute')
-  if (diffMinute > 1) {
+  if (diffMinute >= 1) {
     return `${diffMinute} minutes ago`
   }
 
