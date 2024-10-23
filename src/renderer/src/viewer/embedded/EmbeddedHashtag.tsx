f3114257532a82535a131b79de71e567256239cd
codytseng <codytseng@outlook.com>
2024-10-23 14:48:36 +0800

    update hashtag regex

--- src/renderer/src/viewer/embedded/EmbeddedHashtag.tsx
+++ src/renderer/src/viewer/embedded/EmbeddedHashtag.tsx
@@ -2,7 +2,7 @@ import { EmbeddedHashtag } from '../components/Embedded'
 import { TEmbeddedRenderer } from './types'
 
 export const embeddedHashtagRenderer: TEmbeddedRenderer = {
-  regex: /#(\S+)/g,
+  regex: /#([\w-]+)/g,
   render: (hashtag: string, index: number) => {
     return <EmbeddedHashtag key={`hashtag-${index}`} hashtag={hashtag} />
   }
